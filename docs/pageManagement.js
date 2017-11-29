4d49cdab4c8c661baa58d479b3907c47f30fb1df
jvwthemighty <jakevw123@gmail.com>
2017-11-29 16:46:21 +1000

    Update pageManagement.js

--- docs/pageManagement.js
+++ docs/pageManagement.js
@@ -3,5 +3,11 @@ var run = document.querySelector('#run')
 var output = document.querySelector('#output')
 
 run.onclick = function(){
-  output.value = runJakeScript(input.value)
+  try{
+    output.value = runJakeScript(input.value)
+  }
+  catch(err)
+  {
+    output.value = "You failed to comply with the strict standerds of JakeScript. You are a disgrace. JakeScript was designed for readability and ease of use, and you FAILED. I want nothing to do with you anymore from this day forth."
+  }
 }
